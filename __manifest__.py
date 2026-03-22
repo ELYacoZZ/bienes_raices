c95a7fdb63f8319058a90f750a8949acad36ee38
ELYacoZZ <lazzarettiyc@gmail.com>
2026-03-22 13:39:29 -0400

    19 Actualizaciones

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,11 @@
     'version' : '19.0.1.0.1',
     'license': 'AGPL-3',
     'author' : "Yaco",
+    'data':[
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'data/real_estate_menu.xml',
+    ],
     'application': True,
     
     
